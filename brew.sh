bcc3cec43875db99978c45daffab85e1074bc945
Aldo Solis Zenteno <aldosolis@gmail.com>
2024-11-22 22:11:48 -0600

    Add more commands to .macos and more apps to brew

---
+++ brew.sh
@@ -0,0 +1,24 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+
+brew install code
+brew install visual-studio-code
+brew install slack
+
+brew install neofetch
+
+brew install mise
+echo 'eval "$(/opt/homebrew/bin/mise activate zsh)"' >> ~/.zprofile
+
+brew install 1password
+brew install 1password-cli
+
+brew install awscli
+brew install session-manager-plugin
+
+brew install google-chrome
+brew install appcleaner
+
+brew install obsidian
+
+brew install meetingbar
